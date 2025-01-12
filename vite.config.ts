31e3f6d1a9b06f752fb0175ddbd36840046b20b3
manc1n1 <mancinij1111@gmail.com>
2025-01-12 10:38:07 -0500

    Add GitHub deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/Etch-a-Sketch/',
 	plugins: [react()],
 	resolve: { alias: { '@': path.resolve(__dirname, './src/') } },
 });
